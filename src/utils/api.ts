f44c284be4aa9961a4c5dc317c1f5cba6e901669
ali-khanix <khaniali568@gmail.com>
2025-03-15 20:59:18 +0330

    fixed error

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export const getProducts = async () => {
-  const res = await axios.get('http://localhost:3000/api/products')
+  const res = await axios.get('/api/products')
   return res.data.docs
 }
 
 export const getCategories = async () => {
-  const res = await axios.get('http://localhost:3000/api/categories')
+  const res = await axios.get('/api/categories')
   return res.data.docs
 }
